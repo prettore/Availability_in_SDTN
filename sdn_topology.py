52153ea7f280bd5fa3af310e758f9ec0d4ee723e
Merlin von Rechenberg <merlin.rechenberg@gmail.com>
2021-02-16 12:25:11 +0100

    Create SDN scenarios with simple topology

---
+++ sdn_topology.py
@@ -0,0 +1,100 @@
+import os
+import argparse
+from time import sleep
+import pandas as pd
+
+from mininet.term import makeTerm
+from mininet.log import setLogLevel, info
+from mininet.node import Controller, OVSKernelSwitch
+from mn_wifi.node import OVSKernelAP
+from mn_wifi.link import wmediumd, mesh
+from mn_wifi.cli import CLI
+from mn_wifi.net import Mininet_wifi
+from mn_wifi.wmediumdConnector import interference
+from mn_wifi.replaying import ReplayingMobility
+
+
+def topology(scenario):
+    """Build a custom topology and start it"""
+    net = Mininet_wifi(topo=None, build=False, link=wmediumd, wmediumd_mode=interference, ipBase='10.0.0.0/8')
+
+    info('*** Adding controller\n')
+    c0 = net.addController(name='c0', controller=Controller, protocol='tcp', port=6633)
+
+    info('*** Adding switches/APs\n')
+    ap1 = net.addAccessPoint('ap1', cls=OVSKernelAP, ssid='ap1-ssid', mode='g', channel='1', position='30,50,0')
+
+    info("*** Creating nodes\n")
+    sta1 = net.addStation('sta1', ip='10.0.0.1', position='30,10,0')
+    sta2 = net.addStation('sta2', ip='10.0.0.2', position='10,40,0')
+    sta3 = net.addStation('sta3', ip='10.0.0.3', position='50,40,0')
+
+    info("*** Configuring propagation model\n")
+    net.setPropagationModel(model="logDistance", exp=4.5)
+
+    info("*** Configuring wifi nodes\n")
+    net.configureWifiNodes()
+
+    info("*** Creating wired links\n")
+    net.addLink(sta1, ap1)
+    net.addLink(sta2, ap1)
+    net.addLink(sta3, ap1)
+
+    if scenario == 2:
+        info("*** Configuring moblity\n")
+        trace_file = 'Scenario_2.csv'
+        smooth_motion = False
+        path = os.path.dirname(os.path.abspath(__file__)) + '/data/'
+        get_trace([sta1, sta2, sta3], path+trace_file, smooth_motion)
+        net.isReplaying = True
+
+    info("*** Creating plot\n")
+    net.plotGraph(max_x=100, max_y=100)
+
+    info("*** Starting network\n")
+    net.build()
+    c0.start()
+    net.get('ap1').start([c0])
+    sleep(2)
+    if scenario == 2:
+        info("\n*** Replaying Mobility\n")
+        ReplayingMobility(net)
+
+    makeTerm(sta1, title='Ping_sta2', cmd="ping sta2")
+    makeTerm(sta1, title='Ping_sta3', cmd="ping sta3")
+    info("*** Running CLI\n")
+    CLI(net)
+    net.stop()
+
+
+def get_trace(sta_list, file_, smooth):
+    """Read a trace file"""
+    df_trace = pd.read_csv(file_)
+    df_trace['node'] = df_trace['node'].astype(int)
+    trace_node = df_trace.groupby('node')
+
+    for n in trace_node.groups:
+        sta_list[n].time = []
+        sta_list[n].p = []
+        if smooth:
+            sta_list[n].coord = []
+        trace = trace_node.get_group(n)
+        # for row in trace:
+        for index, row in trace.iterrows():
+            x = row['x']
+            y = row['y']
+            t = row['time']
+            if smooth:
+                sta_list[n].coord.append(str(x) + "," + str(y) + ",0.0")
+            pos = float(x), float(y), 0.0
+            sta_list[n].p.append(pos)
+            sta_list[n].time.append(t)
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    parser = argparse.ArgumentParser(description="Tactical network experiment!")
+    parser.add_argument("-s", "--scenario", help="Select a scenario", type=int, required=True)
+    args = parser.parse_args()
+    scenario = args.scenario
+    topology(scenario)
